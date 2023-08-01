26a252b19deaa614b86aeccbc0d12a6f8ef310d2
@harsha <padma@192.168.1.40>
2023-08-01 19:34:36 +0530

    Github Food ordering system project

--- src/foodordering/Foodordering.java
+++ src/foodordering/Foodordering.java
@@ -15,6 +15,7 @@ public class Foodordering {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello");
     }
     
 }
